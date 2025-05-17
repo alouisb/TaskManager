4aa338c34775b8029b668555b030fac7e32d88b2
marcosenp <marcospne2103@gmail.com>
2025-05-17 10:42:12 -0300

    refactor(program): implementação final do menu principal com integração ao TaskService

--- src/TaskManager/Program.cs
+++ src/TaskManager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TaskManager.Models;
+using TaskManager.Services;
 
 namespace TaskManager
 {
@@ -6,31 +8,72 @@ namespace TaskManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Gerenciador de Tarefas");
-            Console.WriteLine("1 - Adicionar");
-            Console.WriteLine("2 - Listar");
-            Console.WriteLine("3 - Sair");
+            var taskService = new TaskService();
+            bool continuar = true;
 
-            string opcao = Console.ReadLine();
+            while (continuar)
+            {
+                Console.Clear();
+                Console.WriteLine("GERENCIADOR DE TAREFAS");
+                Console.WriteLine("-----------------------");
+                Console.WriteLine("1. Adicionar tarefa");
+                Console.WriteLine("2. Excluir tarefa");
+                Console.WriteLine("3. Editar tarefa");
+                Console.WriteLine("4. Listar tarefas");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+                char opcao = Console.ReadKey().KeyChar;
 
-            if (opcao == "1")
-            {
-                Console.WriteLine("Digite a tarefa:");
-                string tarefa = Console.ReadLine();
-                Console.WriteLine("Tarefa adicionada: " + tarefa);
+                Console.WriteLine("\n");
+
+                switch (opcao)
+                {
+                    case '1':
+                        Console.Write("Informe a descrição da tarefa: ");
+                        string descricao = Console.ReadLine();
+                        taskService.AdicionarTarefa(descricao);
+                        break;
+
+                    case '2':
+                        Console.Write("Informe o ID da tarefa a excluir: ");
+                        int excluirId = int.Parse(Console.ReadLine());
+                        taskService.ExcluirTarefa(excluirId);
+                        break;
+
+                    case '3':
+                        Console.Write("Informe o ID da tarefa a editar: ");
+                        int editarId = int.Parse(Console.ReadLine());
+                        Console.Write("Informe a nova descrição: ");
+                        string novaDescricao = Console.ReadLine();
+                        taskService.EditarTarefa(editarId, novaDescricao);
+                        break;
+
+                    case '4':
+                        var tarefas = taskService.ListarTarefas();
+                        Console.WriteLine("TAREFAS:");
+                        foreach (var tarefa in tarefas)
+                        {
+                            Console.WriteLine($"ID: {tarefa.Id} | Descrição: {tarefa.Descricao}");
+                        }
+                        break;
+
+                    case '5':
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+
+                if (continuar)
+                {
+                    Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+                    Console.ReadKey();
+                }
             }
-            else if (opcao == "2")
-            {
-                Console.WriteLine("Nenhuma tarefa cadastrada.");
-            }
-            else if (opcao == "3")
-            {
-                Console.WriteLine("Saindo...");
-            }
-            else
-            {
-                Console.WriteLine("Opção inválida");
-            }
+
+            Console.WriteLine("Programa encerrado.");
         }
     }
 }
