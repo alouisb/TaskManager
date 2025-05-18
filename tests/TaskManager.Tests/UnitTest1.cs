bd9787f4260407544df8a6805460928c1b772315
marcosenp <marcospne2103@gmail.com>
2025-05-18 11:20:24 -0300

    test(taskservice): adiciona teste unitário básico para adicionar tarefa

--- tests/TaskManager.Tests/UnitTest1.cs
+++ tests/TaskManager.Tests/UnitTest1.cs
@@ -1,10 +1,26 @@
-﻿namespace TaskManager.Tests;
+﻿using Xunit;
+using TaskManager.Services;
 
-public class UnitTest1
+namespace TaskManager.Tests
 {
-    [Fact]
-    public void Test1()
+    public class TaskServiceTests
     {
+        private TaskService _taskService;
+
+        public TaskServiceTests()
+        {
+            _taskService = new TaskService();
+        }
+
+        [Fact]
+        public void AdicionarTarefa_DeveAdicionarTarefaComDescricaoCorreta()
+        {
+            _taskService.AdicionarTarefa("Teste");
+
+            var tarefas = _taskService.ListarTarefas();
+            Assert.Single(tarefas);
+            Assert.Equal("Teste", tarefas[0].Descricao);
+        }
 
     }
 }
